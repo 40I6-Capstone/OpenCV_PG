9726a438752195f86456c7480a55b2d3a22a3f1c
jmorrone14 <46014014+jmorrone14@users.noreply.github.com>
2023-01-11 20:04:56 -0500

    Cleanup
    Cleaned up code and added more comments

--- Colour.py
+++ Colour.py
@@ -1,14 +1,23 @@
+#  Author: Julian Morrone, morronej@mcmaster.ca
+#  This code uses the Python OpenCV library to isolate an oil spill from the surrounding water via photos that will be passed in
+#  It then draws a bounding box around the spill, adds a buffer and creates a discretized circle around it that can be followed by the UGV's to place Bouy's and contain the spill
+
 import cv2
 import numpy as np
 
-cap = cv2.VideoCapture(0) # webcam capture
+# Set Testing to True to see image pipeline
+global testing
+testing = False
+
 cv2.namedWindow("Trackbars", cv2.WINDOW_NORMAL)
 cv2.namedWindow('Original Image', cv2.WINDOW_NORMAL)
-cv2.namedWindow('mask', cv2.WINDOW_NORMAL)
-cv2.namedWindow('mask_result', cv2.WINDOW_NORMAL)
-cv2.namedWindow('Edge', cv2.WINDOW_NORMAL)
 cv2.namedWindow('Contour', cv2.WINDOW_NORMAL)
-cv2.namedWindow('High Contrast', cv2.WINDOW_NORMAL)
+
+if testing:
+    cv2.namedWindow('mask', cv2.WINDOW_NORMAL)
+    cv2.namedWindow('mask_result', cv2.WINDOW_NORMAL)
+    cv2.namedWindow('Edge', cv2.WINDOW_NORMAL)
+    # cv2.namedWindow('High Contrast', cv2.WINDOW_NORMAL)
 
 # try increasing contrast and/or inverting colors to help bounding - don't see much of a difference
 # use contours method to select largest shape from mask result - increase lieniency
@@ -18,7 +27,7 @@ cv2.namedWindow('High Contrast', cv2.WINDOW_NORMAL)
 
 # another potential idea is to make the whole image either black or white based on how close the pixel is to that colour then select the shape from there
 
-def apply_brightness_contrast(input_img, brightness=0, contrast=0):
+def apply_brightness_contrast(input_img, brightness=0, contrast=0): #function to change brightness and contrast in the image
     if brightness != 0:
         if brightness > 0:
             shadow = brightness
@@ -62,10 +71,11 @@ while True:
     frame = cv2.imread("round.jpg")
     frame_high_contrast = apply_brightness_contrast(frame, 0, 20)
     hsv = cv2.cvtColor(frame_high_contrast, cv2.COLOR_BGR2HSV)
-    hsv_high_contrast = apply_brightness_contrast(hsv,0,10)
+
     # min = [0,0,0]
     # max = [180,255,255]
 
+    # Get Lower and Upper HSV+Canny values from the trackbars
     l_h = cv2.getTrackbarPos("L - H", "Trackbars")
     l_s = cv2.getTrackbarPos("L - S", "Trackbars")
     l_v = cv2.getTrackbarPos("L - V", "Trackbars")
@@ -77,18 +87,23 @@ while True:
 
     lower = np.array([l_h, l_s, l_v])
     upper = np.array([u_h, u_s, u_v])
-    mask = cv2.inRange(hsv, lower, upper)
-    invert_mask = cv2.bitwise_not(mask)
 
+    # Mask is applied to hsv of orignal image, using values selected on the trackbar
+    mask = cv2.inRange(hsv, lower, upper)
+    invert_mask = cv2.bitwise_not(mask) # inverted version of the mask
+
+    # mask result (selecting only the oil spill from the original image)
     result = cv2.bitwise_and(frame, frame, mask=invert_mask)
 
+    # apply a Gaussian Blur to make the contours easier to detect
     img_blur = cv2.GaussianBlur(result, (17, 17), 0)
     edge = cv2.Canny(img_blur, canny_lower, canny_upper, L2gradient=True)
 
-    # find the contours in the edged image
+    # find the contours in the inverted mask
     contours, _ = cv2.findContours(invert_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     img_copy = frame.copy()
 
+    # Find the largest Contour and draw a bounding box
     try:
         # find the biggest countour (c) by the area
         c = max(contours, key=cv2.contourArea)
@@ -109,25 +124,26 @@ while True:
         # # draw the largest contour in blue
         cv2.drawContours(img_copy,contours,max_index,blue,15)
 
-    except:
+    except: # Prevents code from crashing when upper and lower limits are all set to 0 (i.e. trackbars not modified)
         print("no contour found")
 
 
     # draw the contours on a copy of the original image
     cv2.drawContours(img_copy, contours, -1, (0, 255, 0), 3)
 
-
-
+    # Display
     cv2.imshow("Original Image", frame)
-    cv2.imshow("High Contrast", frame_high_contrast)
-    cv2.imshow("mask", mask)
-    cv2.imshow("mask_result", result)
-    cv2.imshow('Edge', edge)
     cv2.imshow("Contour", img_copy)
+    if testing:
+        cv2.imshow("High Contrast", frame_high_contrast)
+        cv2.imshow("mask", mask)
+        cv2.imshow("mask_result", result)
+        cv2.imshow('Edge', edge)
 
+    #wait for a key to pressed, if not then close
     key = cv2.waitKey(1)
     if key == 27:
         break
 
-cap.release()
+
 cv2.destroyAllWindows()
